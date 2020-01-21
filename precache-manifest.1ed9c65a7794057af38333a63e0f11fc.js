e7308af512cb26433db656a52cf0aa1d23eccaa8
zemarr <chukwudaluay@gmail.com>
2020-01-21 16:44:45 +0100

    Updates

---
+++ precache-manifest.1ed9c65a7794057af38333a63e0f11fc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ed6d4b4cfdf056965a8bad79fe3687",
+    "url": "/stern-emergency/index.html"
+  },
+  {
+    "revision": "2b41ae86351d62231132",
+    "url": "/stern-emergency/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "82ce0b087f9e464a73cd",
+    "url": "/stern-emergency/static/css/main.046e996d.chunk.css"
+  },
+  {
+    "revision": "2b41ae86351d62231132",
+    "url": "/stern-emergency/static/js/2.f55ae48c.chunk.js"
+  },
+  {
+    "revision": "82ce0b087f9e464a73cd",
+    "url": "/stern-emergency/static/js/main.39d45904.chunk.js"
+  },
+  {
+    "revision": "96ef6caeae705b034343",
+    "url": "/stern-emergency/static/js/runtime-main.7cc637cb.js"
+  },
+  {
+    "revision": "a99f1f46c9cdaf96214661fa9750b84e",
+    "url": "/stern-emergency/static/media/HomeEvents1.a99f1f46.jpg"
+  },
+  {
+    "revision": "77de15f8dec66058a72f10d04d82e27e",
+    "url": "/stern-emergency/static/media/HomeEvents2.77de15f8.jpg"
+  },
+  {
+    "revision": "d16ab001b2428a758bb8181fe6f44fe8",
+    "url": "/stern-emergency/static/media/HomeHeader.d16ab001.jpg"
+  },
+  {
+    "revision": "331b99f3a28019d4ff4c2c815f87a21f",
+    "url": "/stern-emergency/static/media/WorkBgImage.331b99f3.jpg"
+  },
+  {
+    "revision": "6ac124886c04f462bd094b63efdbbad2",
+    "url": "/stern-emergency/static/media/WorkImage.6ac12488.jpg"
+  },
+  {
+    "revision": "8fc1ba2b8ee795b9de6d50b72ec39934",
+    "url": "/stern-emergency/static/media/five.8fc1ba2b.jpg"
+  },
+  {
+    "revision": "fda969a8ed932425a0d5bb2dfe224e12",
+    "url": "/stern-emergency/static/media/four.fda969a8.jpg"
+  },
+  {
+    "revision": "80486f16a26021af514744e5f5b125e8",
+    "url": "/stern-emergency/static/media/logo.80486f16.png"
+  },
+  {
+    "revision": "1f1fdbede867a952eec3183778982dab",
+    "url": "/stern-emergency/static/media/one.1f1fdbed.jpg"
+  },
+  {
+    "revision": "be452f5f696e01fc059b7a68d40495d3",
+    "url": "/stern-emergency/static/media/proffessionalStandard.be452f5f.jpg"
+  },
+  {
+    "revision": "cab2412074234524cadc5ccdee795065",
+    "url": "/stern-emergency/static/media/six.cab24120.jpg"
+  },
+  {
+    "revision": "564328b99522701fd5a69bd27870ed16",
+    "url": "/stern-emergency/static/media/three.564328b9.jpg"
+  },
+  {
+    "revision": "f74319f86226b3a2daf11f2cb1c0fbef",
+    "url": "/stern-emergency/static/media/two.f74319f8.jpg"
+  }
+]);
